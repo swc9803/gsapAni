b02b4ab8e68ac6dcc767d2f883552d77c9c2b24c
swc9803 <swc9803@gmail.com>
2023-12-23 22:36:13 +0900

    gsap를 사용한 페이지 트렌지션

--- app.vue
+++ app.vue
@@ -1,5 +1,70 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <NuxtLayout>
+    <NuxtPage :transition="transition" />
+    <div class="overlay js-mask" />
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import type { TransitionProps } from "vue";
+
+const { $gsap: gsap } = useNuxtApp();
+
+const tl: gsap.core.Timeline = gsap.timeline();
+const transition: TransitionProps = {
+  name: "page",
+  mode: "out-in",
+  onEnter: (el, done) => {
+    tl.to(".js-mask", { left: "200vw" });
+    done();
+  },
+  onBeforeLeave: () => {
+    tl.set(".js-mask", { left: "-200vw" });
+  },
+  onLeave: (el, done) => {
+    tl.to(".js-mask", {
+      left: 0,
+      duration: 0.3,
+    });
+    done();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$transition-mask-color: rgb(0, 59, 220);
+
+.overlay {
+  position: fixed;
+  top: 0;
+  left: -200vw;
+  width: 100vw;
+  height: 100vh;
+  z-index: 9999;
+  background: $transition-mask-color;
+  &:before,
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    width: 100vw;
+    height: 100%;
+  }
+  &:before {
+    right: 100vw;
+    background: linear-gradient(
+      to top left,
+      $transition-mask-color 50%,
+      transparent 50%
+    );
+  }
+  &:after {
+    left: 100vw;
+    background: linear-gradient(
+      to bottom right,
+      $transition-mask-color 50%,
+      transparent 50%
+    );
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
     <p>home</p>
+    <nuxt-link to="test">test</nuxt-link>
   </div>
 </template>
